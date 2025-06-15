5665ee5f71cc1b2abe13fc0d418c56d0bee9849d
danielmartiins <danieldemoraiis@gmail.com>
2025-06-14 22:11:43 -0300

    Criado o token de autenticação do servidor usando  o comando 'openssl rand -hex 64'

--- api/config/config.js
+++ api/config/config.js
@@ -6,4 +6,8 @@ module.exports = {
         password: 'admin',
         databaseName: 'sistema_acoes'
     },
+
+    auth: {
+        tokenKey: "e2d0b5d54a5182af4daa4e2663940d2a39ed65ad7783fa7d81238fc77fcdf8437f5dc7ae311473e57ee6cd73fae3dd84ce53305ce7b7069f36a1d0c09429f707"
+    }
 }
