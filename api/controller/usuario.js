caa7bc15ce10ed87c0260fbf634980634bc81212
danielmartiins <danieldemoraiis@gmail.com>
2025-06-15 18:49:15 -0300

    Corrigido TypeError na variavel usuarioEmail

--- api/controller/usuario.js
+++ api/controller/usuario.js
@@ -19,10 +19,13 @@ router.post('/criarConta', async function (req, res) {
     var senha_hash = await bcrypt.hash(senha, 10);
     var db = await getConnection();
     const resultado = await db.query(
-        'SELECT * FROM usuario WHERE email = ? LIMIT 1;',
+        'SELECT email FROM usuario WHERE email = ?;',
         [email]
     );
-    const usuarioEmail = resultado[0][0];
+
+
+    let usuarioEmail = null;
+    if (resultado) usuarioEmail = resultado;
 
     if (usuarioEmail) {
         res.status(400).json({message: "Já existe um usuário registrado com este e-mail."});
