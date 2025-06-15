dc62589a30b17378cc5d7063c525ff23dafaef39
danielmartiins <danieldemoraiis@gmail.com>
2025-06-15 01:00:09 -0300

    (EM PROGRESSO) Implementado endpoint listaCarteira. Falta mostrar mecanismo de atualização de preços, ganhos e perdas do usuário.

--- api/controller/carteira.js
+++ api/controller/carteira.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const getConnection = require('../model/dbConnection.js');
-
+const auth = require('../auth/auth.js');
 /* 
 * NÃO ESTÁ COMPLETO! 
 * Precisamos implementar a parte de login para que faça a autenticação nesse endpoint. Fiz
@@ -9,16 +9,21 @@ desse jeito mais na intenção de já fazer a consulta
 * Também preciso mostrar algumas informações que ainda não estou mostrando: Mecanismo de atualização de preços, ganhos e perdas do usuário, etc
 */
 router.get('/listaCarteira', async function (req, res) {
-    const id = req.query.id;
+    const claims = auth.verifyToken(req, res);
+    if (!claims) {
+        res.status(401).json({message: "Acesso não autorizado."});
+        return;
+    }
+    let id = claims.user_id;
     let db;
     try {
         db = await getConnection();
+        console.log(claims.user_id)
         let queryString =
             'SELECT ticker, qtde, preco_compra, qtde_vendida, preco_venda FROM acao_carteira WHERE fk_usuario_id = ?';
         let data = [id];
 
         let result = await db.query(queryString, data);
-        await db.end();
         res.json(result);
     } catch (err) {
         console.error(err);
