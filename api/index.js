1301f86898b20420ca801a18d51e0520502fbce1
danielmartiins <danieldemoraiis@gmail.com>
2025-06-14 22:53:28 -0300

    (INCOMPLETO) Desenvolvendo o endpoint de criação de conta

--- api/index.js
+++ api/index.js
@@ -12,4 +12,7 @@ app.use('/olamundo', olaMundo);
 var carteira = require('./controller/carteira.js');
 app.use('/carteira', carteira);
 
+var usuario = require('./controller/usuario.js');
+app.use('/usuario', usuario);
+
 app.listen(3000);
