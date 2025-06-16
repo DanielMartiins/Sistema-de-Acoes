143692bf9ea7cdd87f395c8ea16cba18ab7c9025
danielmartiins <danieldemoraiis@gmail.com>
2025-06-16 15:45:05 -0300

    Adicionado script com constantes para modo de compra e venda, visando padronização

---
+++ api/constants/modo-operacao.js
@@ -0,0 +1,8 @@
+/* 
+Constantes para determinar o modo de compra/venda
+(Se vai ser no preço atual de mercado ou sob determinado valor)
+*/
+module.exports = {
+    MODO_MERCADO: 0, //Para executar compra/venda no preço atual do mercado
+    MODO_VALOR: 1 //Para executar compra/venda ao atingir um valor específico
+}
