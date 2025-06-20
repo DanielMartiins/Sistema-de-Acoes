8aedd695fa0f0b4c1d8a2c2827248752f642ab56
danielmartiins <danieldemoraiis@gmail.com>
2025-06-20 19:57:18 -0300

    Separado trechos repetidos de código em funções para uma melhor organização e modularidade

--- api/controller/acoes/ordemVenda.js
+++ api/controller/acoes/ordemVenda.js
@@ -19,47 +19,39 @@ router.post('/mercado', async function (req, res) {
         return;
     }
 
-    let idUsuario = claims.user_id;
-    let ticker = req.body.ticker;
-    let quantidade = parseInt(req.body.quantidade);
+    const idUsuario = claims.user_id;
+    const ticker = req.body.ticker;
+    const quantidade = parseInt(req.body.quantidade);
+    const erroEntrada = await validarEntrada(
+        idUsuario,
+        ticker,
+        quantidade,
+        MODO_OPERACAO_MERCADO,
+        null
+    );
 
-    if (!ticker || ticker.trim() === '') {
-        res.status(400).json({ message: 'Ticker inválido.' });
-        return;
-    }
-
-    if (!quantidade || quantidade <= 0) {
-        res.status(400).json({ message: 'Quantidade inválida.' });
-        return;
-    }
-
-    if (!(await possuiQuantidadeSuficiente(ticker, quantidade, idUsuario))) {
-        res.status(400).json({
-            message: 'O usuário não possui a quantidade suficiente para realizar a ordem de venda',
-        });
+    if (erroEntrada) {
+        res.status(400).json({ message: erroEntrada });
         return;
     }
 
     try {
         const minutoNegociacao = await obterMinutoNegociacaoUsuario(idUsuario);
         const precoAtual = await obterPrecoMercado(ticker, minutoNegociacao);
-        let db = await getConnection();
 
-        const [insercaoOrdemVenda] = await db.query(
-            ` 
-            CALL registrar_ordem_venda(?, ?, ?, ?, ?);
-            `,
-            [idUsuario, ticker, MODO_OPERACAO_MERCADO, quantidade, precoAtual]
+        //Registrar a ordem de venda no banco de dados
+        const idOrdemVenda = await registrarOrdemVenda(
+            idUsuario,
+            ticker,
+            MODO_OPERACAO_MERCADO,
+            quantidade,
+            precoAtual
         );
-        const idOrdemVenda = insercaoOrdemVenda[0][0].insertId;
 
-        //Executar a ordem de venda
+        //Executar a ordem de venda imediatamente
         await executarOrdemVenda(idUsuario, idOrdemVenda, precoAtual);
 
-        res.json({
-            message: 'Ordem registrada e executada com sucesso.',
-        });
-        await db.end();
+        res.json({ message: 'Ordem registrada e executada com sucesso.' });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: 'Ocorreu um erro no servidor' });
@@ -74,54 +66,42 @@ router.post('/limitada', async function (req, res) {
         return;
     }
 
-    let idUsuario = claims.user_id;
-    let ticker = req.body.ticker;
-    let quantidade = parseInt(req.body.quantidade);
-    let precoReferencia = parseFloat(req.body.precoReferencia);
+    const idUsuario = claims.user_id;
+    const ticker = req.body.ticker;
+    const quantidade = parseInt(req.body.quantidade);
+    const precoReferencia = parseFloat(req.body.precoReferencia);
+    const erroEntrada = await validarEntrada(
+        idUsuario,
+        ticker,
+        quantidade,
+        MODO_OPERACAO_LIMITADA,
+        precoReferencia
+    );
 
-    if (!ticker || ticker.trim() === '') {
-        res.status(400).json({ message: 'Ticker inválido.' });
-        return;
-    }
-
-    if (!quantidade || quantidade <= 0) {
-        res.status(400).json({ message: 'Quantidade inválida.' });
-        return;
-    }
-
-    if (!precoReferencia || precoReferencia <= 0) {
-        res.status(400).json({ message: 'Preço de venda inválido' });
-        return;
-    }
-
-    if (!(await possuiQuantidadeSuficiente(ticker, quantidade, idUsuario))) {
-        res.status(400).json({
-            message: 'O usuário não possui a quantidade suficiente para realizar a ordem de venda',
-        });
+    if (erroEntrada) {
+        res.status(400).json({ message: erroEntrada });
         return;
     }
 
     try {
         const minutoNegociacao = await obterMinutoNegociacaoUsuario(idUsuario);
         const precoAtual = await obterPrecoMercado(ticker, minutoNegociacao);
-        let db = await getConnection();
 
         //Registrar a ordem de venda no banco de dados
-        const [insercaoOrdemVenda] = await db.query(
-            ` 
-            CALL registrar_ordem_venda(?, ?, ?, ?, ?);
-            `,
-            [idUsuario, ticker, MODO_OPERACAO_LIMITADA, quantidade, precoReferencia]
+        const idOrdemVenda = await registrarOrdemVenda(
+            idUsuario,
+            ticker,
+            MODO_OPERACAO_LIMITADA,
+            quantidade,
+            precoReferencia
         );
-
-        const idOrdemVenda = insercaoOrdemVenda[0][0].insertId;
 
         //Se o preço já estiver igual ou acima do desejado, já executa a ordem de venda
         if (await atingiuPrecoDesejadoParaVenda(idUsuario, ticker, precoReferencia)) {
             await executarOrdemVenda(idUsuario, idOrdemVenda, precoAtual);
             res.json({
                 message:
-                    'Ordem realizada com sucesso. O preço de venda já está igual/acima do desejado e a venda também foi executada.',
+                    'Ordem realizada com sucesso. O preço de venda já está igual/acima do desejado e, portanto, a venda também foi executada.',
             });
         } else res.json({ message: 'Ordem de venda registrada com sucesso.' });
     } catch (err) {
@@ -130,9 +110,22 @@ router.post('/limitada', async function (req, res) {
     }
 });
 
+async function validarEntrada(idUsuario, ticker, quantidade, modo, precoReferencia) {
+    if (!ticker || ticker.trim() === '') return 'Ticker inválido.';
+
+    if (!quantidade || quantidade <= 0) return 'Quantidade inválida.';
+
+    if (modo === MODO_OPERACAO_LIMITADA && (!precoReferencia || precoReferencia <= 0))
+        return 'Preço de venda inválido.';
+
+    if (!(await possuiQuantidadeSuficiente(ticker, quantidade, idUsuario)))
+        return 'O usuário não possui a quantidade suficiente para realizar a ordem de venda';
+    return null;
+}
+
 async function possuiQuantidadeSuficiente(ticker, quantidadeVenda, idUsuario) {
-    let db = await getConnection();
-    let consultaQuantidade = await db.query(
+    const db = await getConnection();
+    const consultaQuantidade = await db.query(
         `
         SELECT qtde FROM acao_carteira 
         WHERE fk_usuario_id = ? AND ticker = ? 
@@ -140,18 +133,30 @@ async function possuiQuantidadeSuficiente(ticker, quantidadeVenda, idUsuario) {
         [idUsuario, ticker]
     );
 
-    if (consultaQuantidade[0].length == 0) return false;
+    if (consultaQuantidade[0].length === 0) return false;
 
     const quantidadeDisponivelCarteira = consultaQuantidade[0][0].qtde;
-
-    if (quantidadeDisponivelCarteira.length == 0) return false;
 
     await db.end();
     return quantidadeDisponivelCarteira >= quantidadeVenda;
 }
 
+async function registrarOrdemVenda(idUsuario, ticker, modo, quantidade, precoReferencia) {
+    const db = await getConnection();
+
+    const [insercaoOrdemVenda] = await db.query(
+        `
+        CALL registrar_ordem_venda(?, ?, ?, ?, ?);
+        `,
+        [idUsuario, ticker, modo, quantidade, precoReferencia]
+    );
+
+    const idOrdemVenda = insercaoOrdemVenda[0][0].insertId;
+    return idOrdemVenda;
+}
+
 async function executarOrdemVenda(idUsuario, idOrdemVenda, precoExecucao) {
-    let db = await getConnection();
+    const db = await getConnection();
     try {
         await db.query(
             `
@@ -167,16 +172,7 @@ async function executarOrdemVenda(idUsuario, idOrdemVenda, precoExecucao) {
 
 async function atingiuPrecoDesejadoParaVenda(idUsuario, ticker, precoReferencia) {
     const minutoNegociacao = await obterMinutoNegociacaoUsuario(idUsuario);
-    let response = await axios.get(
-        'https://raw.githubusercontent.com/marciobarros/dsw-simulador-corretora/refs/heads/main/' +
-            minutoNegociacao +
-            '.json'
-    );
-
-    const precoAcoes = response.data;
-    const acaoDesejada = precoAcoes.find((acao) => acao.ticker === ticker);
-
-    if (!acaoDesejada) return false;
-    return acaoDesejada.preco >= precoReferencia;
+    const precoAcaoDesejada = await obterPrecoMercado(ticker, minutoNegociacao);
+    return precoAcaoDesejada >= precoReferencia;
 }
 module.exports = router;
