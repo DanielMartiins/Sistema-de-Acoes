c000c95ccc3d058124232589683d5d08e8daf45b
danielmartiins <danieldemoraiis@gmail.com>
2025-06-20 13:04:12 -0300

    Consertado variavel duplicada (a variavel de ordem de compra tava com nome de usuario, mudei para ordemCompra)

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use('/carteira', carteira);
 var usuario = require('./controller/usuario.js');
 app.use('/usuario', usuario);
 
-var usuario = require('./controller/acoes/ordemCompra.js');
-app.use('/acoes', usuario);
+var ordemCompra = require('./controller/acoes/ordemCompra.js');
+app.use('/acoes', ordemCompra);
 
 app.listen(3000);
