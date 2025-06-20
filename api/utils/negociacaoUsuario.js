78d0f83704ea089da10aad7d02fab942170396b5
danielmartiins <danieldemoraiis@gmail.com>
2025-06-20 15:03:09 -0300

    Adicionado script para obter preço de um ticker do mercado dado o minuto de negociação

---
+++ api/utils/negociacaoUsuario.js
@@ -0,0 +1,19 @@
+const getConnection = require('../model/dbConnection.js');
+
+async function obterMinutoNegociacaoUsuario(idUsuario) {
+    let db = await getConnection();
+    let consultaHoraNegociacao = await db.query(
+        `
+        SELECT MINUTE(ultima_hora_negociacao) as minuto
+        FROM usuario
+        WHERE id = ?
+        `,
+        [idUsuario]
+    );
+    await db.end();
+    return consultaHoraNegociacao[0][0].minuto;
+}
+
+module.exports = {
+    obterMinutoNegociacaoUsuario,
+}
