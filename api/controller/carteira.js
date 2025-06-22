dc731f237b5e6ebcd67b632157652c298bebbf95
danielmartiins <danieldemoraiis@gmail.com>
2025-06-22 12:27:31 -0300

    Implementado endpoint de listar carteira

--- api/controller/carteira.js
+++ api/controller/carteira.js
@@ -3,11 +3,10 @@ var router = express.Router();
 const getConnection = require('../model/dbConnection.js');
 const auth = require('../auth/auth.js');
 const axios = require('axios');
+const { obterMinutoNegociacaoUsuario } = require('../utils/negociacaoUsuario.js');
+const { obterPrecoMercado } = require('../utils/precoMercado.js');
 
-/* Endpoint para listar a carteira
- * NÃO ESTÁ COMPLETO!
- * Preciso mostrar algumas informações que ainda não estou mostrando: Mecanismo de atualização de preços, ganhos e perdas do usuário, etc
- */
+//Listar carteira
 router.get('/', async function (req, res) {
     const claims = auth.verifyToken(req, res);
     if (!claims) {
@@ -16,73 +15,50 @@ router.get('/', async function (req, res) {
     }
 
     try {
-        let id = claims.user_id;
+        let idUsuario = claims.user_id;
         let db = await getConnection();
 
         //Pegar ações da carteira, na tabela acao_carteira do banco de dados
-        let query_acao_carteira = await db.query(
+        const [consulta] = await db.query(
             `
-            SELECT ticker, qtde, preco_compra, qtde_vendida, preco_venda
+            SELECT 
+                ticker, 
+                preco_compra as precoCompra, 
+                preco_venda as precoVenda,
+                qtde, 
+                qtde_vendida as qtdeVendida
             FROM acao_carteira
             WHERE fk_usuario_id = ?
             `,
-            id
+            [idUsuario]
+        ); 
+
+        const minutoNegociacao = await obterMinutoNegociacaoUsuario(idUsuario);
+        const acoesCarteira = await Promise.all(
+            consulta.map((acaoCarteira) =>
+                montarResultadoFinanceiro(acaoCarteira, minutoNegociacao)
+            )
         );
-
-        //await calcularPerdaOuGanho(query_acao_carteira, id);
-
-        await db.end();
-        res.json(query_acao_carteira[0]);
+        res.json(acoesCarteira);
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Ocorreu um erro no servidor' });
     }
 });
 
-//Mecanismo para calcular ganhos do usuário, deixarei de lado por enquanto e farei os outros endpoints
-async function calcularPerdaOuGanho(acoes_carteira, usuario_id) {
-    let db = await getConnection();
-    console.log('Conectou\n');
-
-    let minuto_negociacao = await db.query(
-        `
-        SELECT MINUTE(ultima_hora_negociacao) as minuto FROM usuario WHERE usuario.id = ?;
-        `,
-        [usuario_id]
-    );
-    minuto_negociacao = minuto_negociacao[0].minuto;
-
-    let preco_atual_acoes;
-    await axios
-        .get(
-            'https://raw.githubusercontent.com/marciobarros/dsw-simulador-corretora/refs/heads/main/' +
-                minuto_negociacao +
-                '.json'
-        )
-        .then((response) => {
-            preco_atual_acoes = response.data;
-            console.log('Preco atual das ações recuperado');
-        })
-        .catch((error) => {
-            console.error(error);
-            return;
-        });
-
-    //console.log(preco_atual_acoes);
-    if (!preco_atual_acoes)
-        throw new Error('Não foi possivel retornar as perdas e os ganhos do usuário');
-
-    const tickersCarteira = new Set(acoes_carteira.map((acao) => acao.ticker));
-    preco_atual_acoes = preco_atual_acoes.filter((acao) => tickersCarteira.has(acao.ticker));
-
-    acoes_carteira.forEach(acao => {
-        let qtdeVendida = acao.qtde_vendida;
-        let precoAtual = preco_atual_acoes.find(p => p.ticker === acao) 
-        acao.ganho = acao.preco_venda
-    })
-
-    console.log(preco_atual_acoes);
-    console.log(acoes_carteira);
+//Calcular ganho/perda para uma ação da carteira
+async function montarResultadoFinanceiro(acaoCarteira, minutoNegociacao) {
+    const precoAtualTicker = await obterPrecoMercado(acaoCarteira.ticker, minutoNegociacao);
+    const resultadoFinanceiro = acaoCarteira.qtde * (precoAtualTicker - acaoCarteira.precoCompra);
+    return {
+        ticker: acaoCarteira.ticker,
+        precoMedioCompra: acaoCarteira.precoCompra,
+        precoMedioVenda: acaoCarteira.precoVenda,
+        qtde: acaoCarteira.qtde,
+        qtdeVendida: acaoCarteira.qtdeVendida,
+        resultado: parseFloat(resultadoFinanceiro.toFixed(4)),
+    };
 }
+
 
 module.exports = router;
