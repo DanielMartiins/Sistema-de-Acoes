e73a7cc4b863c2266199965f820d09ae088e4491
danielmartiins <danieldemoraiis@gmail.com>
2025-06-21 22:40:18 -0300

    Adicionado função para obter preço de fechamento de uma ação no mercado dado seu ticker. Também simplificado o código

--- api/utils/precoMercado.js
+++ api/utils/precoMercado.js
@@ -1,19 +1,17 @@
 const axios = require('axios');
 
 async function obterPrecoMercado(ticker, minutoNegociacao) {
+    if (!ticker) throw new Error(`ERRO: Ticker indefinido`);
+    if (ticker.trim() === '') throw new Error(`ERRO: Ticker vazio.`);
 
-    if (!ticker) {
-        throw new Error(`ERRO: Ticker indefinido`)
-    }
-
-    if (ticker.trim() === '') {
-        throw new Error(`ERRO: Ticker vazio!`)
-    }
-
-    if (minutoNegociacao === null 
-        || minutoNegociacao === undefined 
-        || !(minutoNegociacao >= 0 && minutoNegociacao <= 59)) {
-        throw new Error(`ERRO: Minuto inválido! Deve ser um inteiro entre 0 e 59. Valor de minutoNegociação inserido na função: ${minutoNegociacao}`);
+    if (
+        minutoNegociacao === null ||
+        minutoNegociacao === undefined ||
+        !(minutoNegociacao >= 0 && minutoNegociacao <= 59)
+    ) {
+        throw new Error(
+            `ERRO: O minuto "${minutoNegociacao}" é inválido! Deve ser um inteiro entre 0 e 59`
+        );
     }
 
     const url = `https://raw.githubusercontent.com/marciobarros/dsw-simulador-corretora/refs/heads/main/${minutoNegociacao}.json`;
@@ -21,11 +19,23 @@ async function obterPrecoMercado(ticker, minutoNegociacao) {
     const precoAcoes = response.data;
     const acaoDesejada = precoAcoes.find((acao) => acao.ticker === ticker);
 
-    if (!acaoDesejada) throw new Error(`ERRO: Ticker não encontrado. Ticker inserido na função: ${ticker}`);
+    if (!acaoDesejada) throw new Error(`ERRO: Ticker ${ticker} não encontrado.`);
     return acaoDesejada.preco;
 }
 
+async function obterPrecoFechamento(ticker) {
+    if (!ticker) throw new Error(`ERRO: Ticker indefinido.`);
+    if (ticker.trim() === '') throw new Error(`ERRO: Ticker vazio.`);
+
+    const url = `https://raw.githubusercontent.com/marciobarros/dsw-simulador-corretora/refs/heads/main/tickers.json`;
+    let response = await axios.get(url);
+    const precoAcoes = response.data;
+    const acaoDesejada = precoAcoes.find((acao) => acao.ticker === ticker);
+
+    if (!acaoDesejada) throw new Error(`ERRO: Ticker ${ticker} não encontrado.`);
+    return acaoDesejada.fechamento;
+}
 module.exports = {
     obterPrecoMercado,
+    obterPrecoFechamento,
 };
-    
