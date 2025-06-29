3b5ee148076d6eef55cec6113ee95c513fa26813
danielmartiins <danieldemoraiis@gmail.com>
2025-06-29 11:54:46 -0300

    Formatando com prettier

--- api/controller/acoes/atualizaHoraNegociacao.js
+++ api/controller/acoes/atualizaHoraNegociacao.js
@@ -3,7 +3,7 @@ const getConnection = require('../../model/dbConnection');
 const { verifyToken } = require('../../auth/auth');
 const { obterMinutoNegociacaoUsuario } = require('../../utils/negociacaoUsuario');
 const { executarOrdensVenda } = require('../../utils/ordensVenda');
-const { executarOrdensCompra } = require('../../utils/ordensCompra')
+const { executarOrdensCompra } = require('../../utils/ordensCompra');
 const router = express.Router();
 
 router.put('/', async function (req, res) {
@@ -35,36 +35,35 @@ router.put('/', async function (req, res) {
         //Atualizar hora de negociação
         await atualizaHoraNegociacao(idUsuario, novoMinuto);
 
-        //Verificar se há ordens de venda pendentes que estão favoráveis para serem executadas 
+        //Verificar se há ordens de venda pendentes que estão favoráveis para serem executadas
         console.log('Verificando se é possível executar ordem de venda');
         const ordensVendaExecutadas = await executarOrdensVenda(req, res);
         console.log('Retorno:', ordensVendaExecutadas);
 
-        
-        //Verificar se há ordens de compra pendentes que estão favoráveis para serem executadas 
+        //Verificar se há ordens de compra pendentes que estão favoráveis para serem executadas
         console.log('Verificando se é possível executar ordem de compra');
-        const ordensCompraExecutadas = await executarOrdensCompra(req, res); 
+        const ordensCompraExecutadas = await executarOrdensCompra(req, res);
         console.log('Retorno:', ordensCompraExecutadas);
-        
+
         res.json({
             message: `Hora de negociação atualizada para 14:${novoMinuto
                 .toString()
                 .padStart(2, '0')} com sucesso.`,
-                vendasExecutadas: ordensVendaExecutadas,
-                comprasExecutadas: ordensCompraExecutadas
-            });
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ message: 'Ocorreu um erro no servidor.' });
-        }
+            vendasExecutadas: ordensVendaExecutadas,
+            comprasExecutadas: ordensCompraExecutadas,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Ocorreu um erro no servidor.' });
+    }
 });
-    
+
 async function atualizaHoraNegociacao(idUsuario, novoMinuto) {
     const db = await getConnection();
     await db.query(
         `
         UPDATE usuario
-        SET ultima_hora_negociacao = 
+        SET ultima_hora_negociacao =
         TIMESTAMP(
             DATE(ultima_hora_negociacao),
             MAKETIME(
@@ -74,11 +73,9 @@ async function atualizaHoraNegociacao(idUsuario, novoMinuto) {
             )
         )
         WHERE id = ?;
-        `, [novoMinuto, idUsuario]
+        `,
+        [novoMinuto, idUsuario]
     );
 }
-
-
-
 
 module.exports = router;
