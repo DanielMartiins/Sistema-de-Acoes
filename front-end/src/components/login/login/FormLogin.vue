8a43c645421f9c62f130eb2ed97aaea29d724556
DanielMartiins <danieldemoraiis@gmail.com>
2025-07-03 13:43:31 -0300

    Adicionado tela de login (não-funcional, apenas a interface feita com vuetify)

--- front-end/src/components/login/login/FormLogin.vue
+++ front-end/src/components/login/login/FormLogin.vue
@@ -1,7 +1,18 @@
 <template>
-    <v-container> </v-container>
+    <v-container :width="300">
+        <v-form>
+            <v-text-field label="Email" v-model="form.email"></v-text-field>
+            <v-text-field label="Senha" v-model="form.senha"></v-text-field>
+            <v-btn>Entrar</v-btn>
+        </v-form>
+    </v-container>
 </template>
 
-<script>
-export default {};
+<script setup>
+    import { ref } from 'vue'
+
+    const form = ref({
+        email: "",
+        senha: ""
+    })
 </script>
