947ae9e883843e6240384baeabdaaa5716ec5c73
DanielMartiins <danieldemoraiis@gmail.com>
2025-07-03 13:26:54 -0300

    Adicionado tema escuro e cores pro primary, secondary e background

--- front-end/src/plugins/vuetify.js
+++ front-end/src/plugins/vuetify.js
@@ -5,6 +5,19 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+    theme: {
+        defaultTheme: 'dark',
+        themes: {
+            dark: {
+                dark: true,
+                colors: {
+                    primary: '#1E88E5',
+                    secondary: '#424242',
+                    background: '##121212',
+                }
+            }
+        }
+    }
+})
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
