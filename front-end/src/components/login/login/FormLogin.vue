82371a69161adeb4806965bbccfee11de25c7f91
DanielMartiins <danieldemoraiis@gmail.com>
2025-07-04 15:07:21 -0300

    Melhorado os avisos da tela de login

--- front-end/src/components/login/login/FormLogin.vue
+++ front-end/src/components/login/login/FormLogin.vue
@@ -1,38 +1,46 @@
 <template>
     <v-app>
         <!-- WRAPPER para centralizar -->
-        <div class="d-flex justify-center align-center" style="height: 100vh">
-            <v-container class="rounded-xl" :width="400" style="background-color: #212121">
-                <h2 class="text-h5 text-center mb-4">Login</h2>
-                <v-form>
-                    <v-text-field label="Email" v-model="form.email"></v-text-field>
-                    <v-text-field label="Senha" v-model="form.senha"></v-text-field>
+        <div class="d-flex flex-column justify-center align-center" style="height: 100vh">
+            <div>
+                <h4 class="text-h4 text-center mb-4">Login</h4>
 
-                    <div class="d-flex align-center justify-sm-space-between">
-                        <v-btn color="indigo-darken-2" @click="processarLogin">Entrar</v-btn>
+                <v-alert
+                    v-if="loginBemSucedido === false && falhaNoServidor === false"
+                    type="error"
+                    class="mb-4"
+                    title="Email ou senha inválidos"
+                    timeout="3s"
+                    style="max-width: 400px"
+                />
+                <v-alert
+                    v-if="falhaNoServidor === true"
+                    type="error"
+                    class="mb-4"
+                    title="Ocorreu um erro no servidor"
+                    style="max-width: 400px"
+                />
 
-                        <div
-                            class="text-error"
-                            v-if="loginBemSucedido === false && !falhaNoServidor"
-                        >
-                            Email ou senha incorretos
+                <v-alert
+                    v-if="loginBemSucedido === true"
+                    type="success"
+                    class="mb-4"
+                    title="Login realizado com sucesso"
+                    style="max-width: 400px"
+                />
+
+                <v-container class="rounded-lg" :width="400" style="background-color: #212121">
+                    <v-form>
+                        <v-text-field label="Email" v-model="form.email"></v-text-field>
+                        <v-text-field label="Senha" v-model="form.senha"></v-text-field>
+
+                        <div class="d-flex align-center justify-sm-space-between">
+                            <v-btn color="indigo-darken-2" @click="processarLogin">Entrar</v-btn>
                         </div>
-                        <div class="text-error" v-if="falhaNoServidor === true">
-                            Ocorreu um erro no servidor
-                        </div>
-                    </div>
-                </v-form>
-            </v-container>
+                    </v-form>
+                </v-container>
+            </div>
         </div>
-
-        <v-dialog v-model="loginBemSucedido" location="center" persistent width="320">
-            <v-card title="Login Bem Sucedido">
-                <v-card-text>Login realizado com sucesso!</v-card-text>
-                <v-card-actions>
-                    <v-btn @click="loginBemSucedido = null">Fechar</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
     </v-app>
 </template>
 
@@ -51,7 +59,6 @@ const form = ref({
 const loginBemSucedido = ref(null);
 const falhaNoServidor = ref(false);
 function processarLogin() {
-    let error = '';
     axios
         .post(
             `${config.url}/usuario/login`,
@@ -72,9 +79,8 @@ function processarLogin() {
         })
         .catch((err) => {
             console.log(err.response);
-            error = err;
             loginBemSucedido.value = false;
-            if (err.response.status === 500) falhaNoServidor.value = true;
+            if (!err.response || err.response.status === 500) falhaNoServidor.value = true;
             else falhaNoServidor.value = false;
         });
 }
