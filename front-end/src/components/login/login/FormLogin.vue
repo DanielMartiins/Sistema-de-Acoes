ca0b9cdee466a8aec9da90faf129ea99877f6ce2
DanielMartiins <danieldemoraiis@gmail.com>
2025-07-04 15:21:11 -0300

    Corrigindo indentação

--- front-end/src/components/login/login/FormLogin.vue
+++ front-end/src/components/login/login/FormLogin.vue
@@ -1,47 +1,47 @@
 <template>
-    <v-app>
-        <!-- WRAPPER para centralizar -->
-        <div class="d-flex flex-column justify-center align-center" style="height: 100vh">
-            <div>
-                <h4 class="text-h4 text-center mb-4">Login</h4>
+  <v-app>
+    <!-- WRAPPER para centralizar -->
+    <div class="d-flex flex-column justify-center align-center" style="height: 100vh">
+      <div>
+        <h4 class="text-h4 text-center mb-4">Login</h4>
 
-                <v-alert
-                    v-if="loginBemSucedido === false && falhaNoServidor === false"
-                    type="error"
-                    class="mb-4"
-                    title="Email ou senha inválidos"
-                    timeout="3s"
-                    style="max-width: 400px"
-                />
-                <v-alert
-                    v-if="falhaNoServidor === true"
-                    type="error"
-                    class="mb-4"
-                    title="Ocorreu um erro no servidor"
-                    style="max-width: 400px"
-                />
+        <v-alert
+          v-if="loginBemSucedido === false && falhaNoServidor === false"
+          type="error"
+          class="mb-4"
+          title="Email ou senha inválidos"
+          timeout="3s"
+          style="max-width: 400px"
+        />
+        <v-alert
+          v-if="falhaNoServidor === true"
+          type="error"
+          class="mb-4"
+          title="Ocorreu um erro no servidor"
+          style="max-width: 400px"
+        />
 
-                <v-alert
-                    v-if="loginBemSucedido === true"
-                    type="success"
-                    class="mb-4"
-                    title="Login realizado com sucesso"
-                    style="max-width: 400px"
-                />
+        <v-alert
+          v-if="loginBemSucedido === true"
+          type="success"
+          class="mb-4"
+          title="Login realizado com sucesso"
+          style="max-width: 400px"
+        />
 
-                <v-container class="rounded-lg" :width="400" style="background-color: #212121">
-                    <v-form>
-                        <v-text-field label="Email" v-model="form.email"></v-text-field>
-                        <v-text-field label="Senha" v-model="form.senha"></v-text-field>
+        <v-container class="rounded-lg" :width="400" style="background-color: #212121">
+          <v-form>
+            <v-text-field label="Email" v-model="form.email"></v-text-field>
+            <v-text-field label="Senha" v-model="form.senha"></v-text-field>
 
-                        <div class="d-flex align-center justify-sm-space-between">
-                            <v-btn color="indigo-darken-2" @click="processarLogin">Entrar</v-btn>
-                        </div>
-                    </v-form>
-                </v-container>
+            <div class="d-flex align-center justify-sm-space-between">
+              <v-btn color="indigo-darken-2" @click="processarLogin">Entrar</v-btn>
             </div>
-        </div>
-    </v-app>
+          </v-form>
+        </v-container>
+      </div>
+    </div>
+  </v-app>
 </template>
 
 <script setup>
@@ -53,35 +53,35 @@ const credentials = appContext.config.globalProperties.credentials;
 const config = appContext.config.globalProperties.config;
 
 const form = ref({
-    email: 'danieldemoraiis@gmail.com',
-    senha: 'teste123',
+  email: 'danieldemoraiis@gmail.com',
+  senha: 'teste123',
 });
 const loginBemSucedido = ref(null);
 const falhaNoServidor = ref(false);
 function processarLogin() {
-    axios
-        .post(
-            `${config.url}/usuario/login`,
-            {
-                email: form.value.email,
-                senha: form.value.senha,
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-            }
-        )
-        .then((response) => {
-            credentials.value = response.data.token;
-            loginBemSucedido.value = true;
-            falhaNoServidor.value = false;
-        })
-        .catch((err) => {
-            console.log(err.response);
-            loginBemSucedido.value = false;
-            if (!err.response || err.response.status === 500) falhaNoServidor.value = true;
-            else falhaNoServidor.value = false;
-        });
+  axios
+    .post(
+      `${config.url}/usuario/login`,
+      {
+        email: form.value.email,
+        senha: form.value.senha,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+    .then((response) => {
+      credentials.value = response.data.token;
+      loginBemSucedido.value = true;
+      falhaNoServidor.value = false;
+    })
+    .catch((err) => {
+      console.log(err.response);
+      loginBemSucedido.value = false;
+      if (!err.response || err.response.status === 500) falhaNoServidor.value = true;
+      else falhaNoServidor.value = false;
+    });
 }
 </script>
