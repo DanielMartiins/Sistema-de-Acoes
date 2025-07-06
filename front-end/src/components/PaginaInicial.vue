1c8b2126a86765e80a0a10186af8e935d5d5317a
DanielMartiins <danieldemoraiis@gmail.com>
2025-07-06 00:27:00 -0300

    Reorganizando projeto, separando view e components adequadamente

--- front-end/src/components/PaginaInicial.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  Essa será a página que o usuário verá as ações do mercado (por padrão são geradas 10 ações para serem mostradas quando ele registra uma nova conta e ele pode remover/adicionar quantas ele quiser).
-</template>
---
+++ front-end/src/views/PaginaInicial.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-app>
+      <NavegadorSidebar/>
+    </v-app>
+</template>
+
+<script setup>
+
+// Components
+import NavegadorSidebar from '@/components/NavegadorSidebar.vue';
+</script>
