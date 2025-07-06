20a978670bbe47ba16cb2f6b0fbca92b7eed5a0f
DanielMartiins <danieldemoraiis@gmail.com>
2025-07-06 00:25:09 -0300

    Adicionado componente de navegação via sidebar. OBS: Incompleto, é um template do vuetify

---
+++ front-end/src/components/NavegadorSidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout>
+    <v-navigation-drawer expand-on-hover rail permanent>
+      <v-list>
+        <v-list-item prepend-icon="mdi-account" subtitle="fulanociclano@gmail.com"></v-list-item>
+      </v-list>
+
+      <v-divider />
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account-multiple"
+          title="Shared with me"
+          value="shared"
+        ></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
+</template>
