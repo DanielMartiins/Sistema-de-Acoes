da27a24fa0dd6693e43a568689deda6a26d47468
DanielMartiins <danieldemoraiis@gmail.com>
2025-07-06 17:47:02 -0300

    Implementado logout

--- front-end/src/components/NavegadorSidebar.vue
+++ front-end/src/components/NavegadorSidebar.vue
@@ -1,27 +1,50 @@
 <template>
   <v-layout>
     <v-navigation-drawer expand-on-hover rail permanent>
-      <v-list>
-        <v-list-item prepend-icon="mdi-account" :subtitle="user.email"></v-list-item>
-      </v-list>
+      <v-expansion-panels elevation="0" class="mt-2 mb-2">
+        <v-expansion-panel>
+          <v-expansion-panel-title class="pa-0">
+            <v-list-item density="compact" class="pe-2" style="height: 48px">
+              <template #prepend>
+                <v-icon>mdi-account</v-icon>
+              </template>
+
+              <div v-if="user" style="white-space: nowrap; text-overflow: ellipsis">
+                <v-list-item-title>Minha Conta</v-list-item-title>
+                <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
+              </div>
+            </v-list-item>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text class="pa-0">
+            <v-list density="compact" nav class="pa-0">
+              <v-list-item @click="handleLogout" title="Sair" prepend-icon="mdi-logout" value="logout"></v-list-item>
+            </v-list>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
 
       <v-divider />
 
       <v-list density="compact" nav>
-        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
         <v-list-item
-          prepend-icon="mdi-account-multiple"
-          title="Shared with me"
-          value="shared"
+          prepend-icon="mdi-wallet-bifold"
+          title="Minha carteira"
+          value="minhacarteira"
         ></v-list-item>
-        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
       </v-list>
     </v-navigation-drawer>
   </v-layout>
 </template>
 
 <script setup>
-  import { useAuth } from '@/composables/useAuth';
+import { useAuth } from '@/composables/useAuth';
+import { useRouter } from 'vue-router';
 
-  const { user } = useAuth();
+const { user, logout } = useAuth();
+const router = useRouter();
+
+function handleLogout() {
+  logout();
+  router.push({name: 'login'})
+}
 </script>
