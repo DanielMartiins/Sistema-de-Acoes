06ce0585735e30a3220bc44a19025c0972e17e9b
DanielMartiins <danieldemoraiis@gmail.com>
2025-07-08 00:09:54 -0300

    feat, style: adicionar aviso de tabela vazia e refinado estilo do diálogo de remoção de ação

--- front-end/src/views/PaginaInicial.vue
+++ front-end/src/views/PaginaInicial.vue
@@ -2,10 +2,10 @@
   <NavegadorSidebar />
   <div class="tabela-wrapper">
     <v-container class="pa-0 tabela-container">
-      <h4 class="text-h3 text-center ma-2">Ações de Interesse</h4>
+      <h4 class="text-h3 text-center ma-2">Mercado de Ações</h4>
       <div class="tabela-box">
         <!-- Cabeçalho fixo -->
-        <v-row class="bg-primary font-weight-bold" no-gutters>
+        <v-row class="bg-primary font-weight-bold pa-2 pt-1 pb-1" no-gutters>
           <v-col cols="2">Ticker</v-col>
           <v-col cols="2" class="text-end">Preço</v-col>
           <v-col class="text-end">Variação $</v-col>
@@ -16,6 +16,7 @@
         <!-- Parte rolável -->
         <div class="scroll-tabela d-flex flex-column">
           <LinhaAcaoInteresse
+            class="pl-2 pr-2"
             @removerAcao="abrirDialogoRemocao"
             v-for="(acao, i) in acoes"
             :key="i"
@@ -23,11 +24,22 @@
             :index="i"
           />
         </div>
+
+        <div class="d-flex w-100 h-100" v-if="acoes.length === 0">
+          <v-card class="align-center pa-5 justify-center text-center w-100 h-100">
+            <v-icon size="75px">mdi-alert-box-outline</v-icon>
+            <v-card-title>Ops!</v-card-title>
+            <v-card-text
+              >Parece que você não possui ações de interesse para serem exibidas. <br />Adicione
+              ações de interesse para vê-las nessa lista</v-card-text
+            >
+          </v-card>
+        </div>
       </div>
     </v-container>
   </div>
-  <v-dialog v-model="dialogoRemocao">
-    <v-card rounded="lg" max-width="400" class="align-center ma-auto">
+  <v-dialog v-model="dialogoRemocao" id="dialogo-remocao">
+    <v-card rounded="lg" max-width="400" class="align-center ma-auto" elevation="24">
       <v-icon class="ma-5" color="warning" size="100px"> mdi-alert-outline </v-icon>
       <v-card-title>Remover ação da lista</v-card-title>
       <v-card-text class="text-center">
@@ -39,7 +51,7 @@
       <v-divider></v-divider>
       <v-card-actions>
         <v-btn @click="dialogoRemocao = false">Cancelar</v-btn>
-        <v-btn @click="removerAcao" prepend-icon="mdi-delete" color="error">Remover</v-btn>
+        <v-btn variant="outlined" @click="removerAcao" prepend-icon="mdi-delete" color="error">Remover</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -68,7 +80,7 @@ async function removerAcao() {
       },
     });
 
-    acoes.value.splice(indexParaRemover.value, 1)
+    acoes.value.splice(indexParaRemover.value, 1);
     dialogoRemocao.value = false;
     return true;
   } catch (err) {
@@ -117,13 +129,14 @@ onMounted(async () => {
 }
 
 .tabela-box {
-  border: 2px solid #555;
+  border: 2px solid #3f51b5;
   height: 260px;
   overflow: hidden;
+  border-radius: 5px;
 }
 
 .scroll-tabela {
-  max-height: 232px;
+  max-height: 224px;
   overflow-y: auto;
 }
 
